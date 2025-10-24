b324569b18fc2b58cfc062b5520aa17c99673d6f
Tariq Alagha <tariqalagha02@gmail.com>
2025-10-24 14:55:00 -0400

    added main event loop with cell coloring at mouse hover location

--- fluid.c
+++ fluid.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include "SDL_events.h"
+#include "SDL_surface.h"
 #include <SDL2/SDL.h>
+#include <stdio.h>
 
 #define SCREEN_WIDTH 900
 #define SCREEN_HEIGHT 600
@@ -7,48 +9,63 @@
 #define COLOR_BLUE 0x4287f5
 #define COLOR_GRAY 0x1f1f1f1f
 #define CELL_SIZE 20
-#define COLUMNS SCREEN_WIDTH/CELL_SIZE
-#define ROWS SCREEN_HEIGHT/CELL_SIZE
+#define COLUMNS SCREEN_WIDTH / CELL_SIZE
+#define ROWS SCREEN_HEIGHT / CELL_SIZE
 #define LINE_WIDTH 2
 
-
-
-struct Cell
-{
-    int type;
-    int fill_level;
-
+struct Cell {
+  int type;
+  int fill_level;
 };
+void color_cell(SDL_Surface *surface, int x, int y) {
+  int pixel_x = x * CELL_SIZE;
+  int pixel_y = y * CELL_SIZE;
 
-void draw_grid(SDL_Surface* surface)
-{
-
-    for (int i=0; i<COLUMNS; i++)
-    {
-        SDL_Rect column = (SDL_Rect) {i*CELL_SIZE, 0, LINE_WIDTH, SCREEN_HEIGHT};
-        SDL_FillRect(surface, &column, COLOR_GRAY);
-    }
-    for (int j=0; j<ROWS; j++)
-    {
-        SDL_Rect row = (SDL_Rect) {0, j*CELL_SIZE, SCREEN_WIDTH, LINE_WIDTH};
-        SDL_FillRect(surface, &row, COLOR_GRAY);
-    }
+  SDL_Rect cell_rect = (SDL_Rect){pixel_x, pixel_y, CELL_SIZE, CELL_SIZE};
+  SDL_FillRect(surface, &cell_rect, COLOR_WHITE);
 }
 
-int main()
-{
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Fluid Sim", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    SDL_Surface* surface = SDL_GetWindowSurface(window);
-    draw_grid(surface);
+void draw_grid(SDL_Surface *surface) {
 
-    SDL_Rect rectangle = (SDL_Rect){50,50,100,50};
+  for (int i = 0; i < COLUMNS; i++) {
+    SDL_Rect column = (SDL_Rect){i * CELL_SIZE, 0, LINE_WIDTH, SCREEN_HEIGHT};
+    SDL_FillRect(surface, &column, COLOR_GRAY);
+  }
+  for (int j = 0; j < ROWS; j++) {
+    SDL_Rect row = (SDL_Rect){0, j * CELL_SIZE, SCREEN_WIDTH, LINE_WIDTH};
+    SDL_FillRect(surface, &row, COLOR_GRAY);
+  }
+}
+
+int main() {
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window *window =
+      SDL_CreateWindow("Fluid Sim", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+  SDL_Surface *surface = SDL_GetWindowSurface(window);
+  draw_grid(surface);
+
+  int simulation_running = 1;
+
+  SDL_Event event;
+  while (simulation_running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        simulation_running = 0;
+      }
+      if (event.type == SDL_MOUSEMOTION) {
+        int cell_x = event.motion.x / CELL_SIZE;
+        int cell_y = event.motion.y / CELL_SIZE;
+        color_cell(surface, cell_x, cell_y);
+      }
+    }
+    SDL_Rect rectangle = (SDL_Rect){50, 50, 100, 50};
     SDL_FillRect(surface, &rectangle, COLOR_WHITE);
 
-    SDL_Rect blue_rectangle = (SDL_Rect){150,50,100,50};
+    SDL_Rect blue_rectangle = (SDL_Rect){150, 50, 100, 50};
     SDL_FillRect(surface, &blue_rectangle, COLOR_BLUE);
-    
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(3000);
 
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(100);
+  }
 }
