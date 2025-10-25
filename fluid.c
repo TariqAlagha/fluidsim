b3a1ddbeb2e5472a4a61761725ccbce82d8bcf00
Tariq Alagha <tariqalagha02@gmail.com>
2025-10-24 21:48:37 -0400

    add ability to draw blue cells, drawing only occurs when mouse button is pressed.

--- fluid.c
+++ fluid.c
@@ -1,7 +1,8 @@
 #include "SDL_events.h"
+#include "SDL_keycode.h"
+#include "SDL_stdinc.h"
 #include "SDL_surface.h"
 #include <SDL2/SDL.h>
-#include <stdio.h>
 
 #define SCREEN_WIDTH 900
 #define SCREEN_HEIGHT 600
@@ -12,17 +13,25 @@
 #define COLUMNS SCREEN_WIDTH / CELL_SIZE
 #define ROWS SCREEN_HEIGHT / CELL_SIZE
 #define LINE_WIDTH 2
+#define SOLID_TYPE 1
+#define WATER_TYPE 0
 
 struct Cell {
   int type;
   int fill_level;
+  int x;
+  int y;
 };
-void color_cell(SDL_Surface *surface, int x, int y) {
-  int pixel_x = x * CELL_SIZE;
-  int pixel_y = y * CELL_SIZE;
+void draw_cell(SDL_Surface *surface, struct Cell cell) {
+  int pixel_x = cell.x * CELL_SIZE;
+  int pixel_y = cell.y * CELL_SIZE;
 
   SDL_Rect cell_rect = (SDL_Rect){pixel_x, pixel_y, CELL_SIZE, CELL_SIZE};
-  SDL_FillRect(surface, &cell_rect, COLOR_WHITE);
+  Uint32 color = COLOR_WHITE;
+  if (cell.type == WATER_TYPE) {
+    color = COLOR_BLUE;
+  }
+  SDL_FillRect(surface, &cell_rect, color);
 }
 
 void draw_grid(SDL_Surface *surface) {
@@ -48,22 +57,26 @@ int main() {
   int simulation_running = 1;
 
   SDL_Event event;
+  int current_type = SOLID_TYPE;
   while (simulation_running) {
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
         simulation_running = 0;
       }
       if (event.type == SDL_MOUSEMOTION) {
-        int cell_x = event.motion.x / CELL_SIZE;
-        int cell_y = event.motion.y / CELL_SIZE;
-        color_cell(surface, cell_x, cell_y);
+        if (event.motion.state != 0) {
+          int cell_x = event.motion.x / CELL_SIZE;
+          int cell_y = event.motion.y / CELL_SIZE;
+          struct Cell cell = (struct Cell){current_type, 0, cell_x, cell_y};
+          draw_cell(surface, cell);
+        }
+      }
+      if (event.type == SDL_KEYDOWN) {
+        if (event.key.keysym.sym == SDLK_SPACE) {
+          current_type = !current_type;
+        }
       }
     }
-    SDL_Rect rectangle = (SDL_Rect){50, 50, 100, 50};
-    SDL_FillRect(surface, &rectangle, COLOR_WHITE);
-
-    SDL_Rect blue_rectangle = (SDL_Rect){150, 50, 100, 50};
-    SDL_FillRect(surface, &blue_rectangle, COLOR_BLUE);
 
     SDL_UpdateWindowSurface(window);
     SDL_Delay(100);
