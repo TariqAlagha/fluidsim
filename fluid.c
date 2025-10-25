e5338ae2e75d54a40e75510bd3831a4bb7d437cc
Tariq Alagha <tariqalagha02@gmail.com>
2025-10-24 23:10:05 -0400

    allow for switching betwen solid, water, and delete, all with a backend model of the simulation so we're actually placing cell in the backend model

--- fluid.c
+++ fluid.c
@@ -1,5 +1,6 @@
 #include "SDL_events.h"
 #include "SDL_keycode.h"
+#include "SDL_rect.h"
 #include "SDL_stdinc.h"
 #include "SDL_surface.h"
 #include <SDL2/SDL.h>
@@ -9,6 +10,7 @@
 #define COLOR_WHITE 0xffffffff
 #define COLOR_BLUE 0x4287f5
 #define COLOR_GRAY 0x1f1f1f1f
+#define COLOR_BLACK 0x00000000
 #define CELL_SIZE 20
 #define COLUMNS SCREEN_WIDTH / CELL_SIZE
 #define ROWS SCREEN_HEIGHT / CELL_SIZE
@@ -18,20 +20,35 @@
 
 struct Cell {
   int type;
-  int fill_level;
+  /* between 0 (empty) and 1 (full) */
+  double fill_level;
   int x;
   int y;
 };
 void draw_cell(SDL_Surface *surface, struct Cell cell) {
   int pixel_x = cell.x * CELL_SIZE;
   int pixel_y = cell.y * CELL_SIZE;
-
   SDL_Rect cell_rect = (SDL_Rect){pixel_x, pixel_y, CELL_SIZE, CELL_SIZE};
-  Uint32 color = COLOR_WHITE;
+  // background color
+  SDL_FillRect(surface, &cell_rect, COLOR_BLACK);
+  // water fill level
   if (cell.type == WATER_TYPE) {
-    color = COLOR_BLUE;
+    int water_height = cell.fill_level * CELL_SIZE;
+    int empty_height = CELL_SIZE - water_height;
+    SDL_Rect water_rect =
+        (SDL_Rect){pixel_x, pixel_y + empty_height, CELL_SIZE, water_height};
+    SDL_FillRect(surface, &water_rect, COLOR_BLUE);
   }
-  SDL_FillRect(surface, &cell_rect, color);
+  // solid blocks
+  if (cell.type == SOLID_TYPE) {
+    SDL_FillRect(surface, &cell_rect, COLOR_WHITE);
+  }
+}
+void draw_environment(SDL_Surface *surface,
+                      struct Cell environment[ROWS * COLUMNS]) {
+  for (int i = 0; i < ROWS * COLUMNS; i++) {
+    draw_cell(surface, environment[i]);
+  }
 }
 
 void draw_grid(SDL_Surface *surface) {
@@ -45,6 +62,13 @@ void draw_grid(SDL_Surface *surface) {
     SDL_FillRect(surface, &row, COLOR_GRAY);
   }
 }
+void initialize_environment(struct Cell environment[ROWS * COLUMNS]) {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      environment[j + COLUMNS * i] = (struct Cell){WATER_TYPE, 0, j, i};
+    }
+  }
+}
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
@@ -52,12 +76,15 @@ int main() {
       SDL_CreateWindow("Fluid Sim", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
   SDL_Surface *surface = SDL_GetWindowSurface(window);
-  draw_grid(surface);
+  // model the cell grid
+  struct Cell environment[ROWS * COLUMNS];
+  initialize_environment(environment);
 
   int simulation_running = 1;
 
   SDL_Event event;
   int current_type = SOLID_TYPE;
+  int delete_mode = 0;
   while (simulation_running) {
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
@@ -67,17 +94,26 @@ int main() {
         if (event.motion.state != 0) {
           int cell_x = event.motion.x / CELL_SIZE;
           int cell_y = event.motion.y / CELL_SIZE;
-          struct Cell cell = (struct Cell){current_type, 0, cell_x, cell_y};
-          draw_cell(surface, cell);
+          int fill_level = delete_mode ? 0 : 1;
+          if (delete_mode != 0) {
+            current_type = WATER_TYPE;
+          }
+          struct Cell cell =
+              (struct Cell){current_type, fill_level, cell_x, cell_y};
+          environment[cell_x + COLUMNS * cell_y] = cell;
         }
       }
       if (event.type == SDL_KEYDOWN) {
         if (event.key.keysym.sym == SDLK_SPACE) {
           current_type = !current_type;
         }
+        if (event.key.keysym.sym == SDLK_BACKSPACE) {
+          delete_mode = !delete_mode;
+        }
       }
     }
-
+    draw_environment(surface, environment);
+    draw_grid(surface);
     SDL_UpdateWindowSurface(window);
     SDL_Delay(100);
   }
